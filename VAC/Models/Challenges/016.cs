58014844993691861689f8e1b05364575cb1be4f
neo <neotoxic.off@gmail.com>
2025-01-30 09:13:10 +0100

    Create 016.cs

---
+++ VAC/Models/Challenges/016.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VAC.Interfaces;
+
+namespace VAC.Models.Characters
+{
+    public class _016 : IChallenge
+    {
+        public string Name { get; set; } = "All players must walk during the entire round";
+        public IChallenge.Types Type { get; set; } = IChallenge.Types.Team;
+        public uint Rounds { get; set; } = 2;
+    }
+}
