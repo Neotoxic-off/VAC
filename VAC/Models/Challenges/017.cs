0ce7d031769354a9c46b80956726936f29510305
neo <neotoxic.off@gmail.com>
2025-01-30 13:10:52 +0100

    Create 017.cs

---
+++ VAC/Models/Challenges/017.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VAC.Interfaces;
+
+namespace VAC.Models.Characters
+{
+    public class _017 : IChallenge
+    {
+        public string Name { get; set; } = "All players must play judge or bucky";
+        public IChallenge.Types Type { get; set; } = IChallenge.Types.Team;
+        public uint Rounds { get; set; } = 2;
+    }
+}
